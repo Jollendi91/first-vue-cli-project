2d1af0e914ffca886bae13fe52de4ef64510608a
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-04 11:12:02 -0600

    refactor footer

--- src/Footer.vue
+++ src/Footer.vue
@@ -1,7 +1,11 @@
 <template>
-    <footer>
-        <p>{{ footer }}</p>
-    </footer>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer>
+                <p>{{ footer }}</p>
+            </footer>
+        </div>
+    </div> 
 </template>
 
 <script>
